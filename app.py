2314963a3ca81a8140a60620c460c40a8afa84e9
Tamirys Pino <taamypino@gmail.com>
2020-08-06 19:25:22 +0200

    Paginating authors

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from resources.errors import errors
 from database.db import initialize_db
 from flask import Flask
-from flask_mail import Mail
 from flask_restful import Api
 
-# imports requiring app and mail
 from resources.routes import initialize_routes
 
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
-mail = Mail(app)
-
 
 api = Api(app, errors=errors)
 
--- resources/author.py
+++ resources/author.py
@@ -1,12 +1,12 @@
-from flask import Response, request
+from flask import Response, request, jsonify
 from database.models import Author
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
-
+from flask_mongoengine import Pagination
 from mongoengine.errors import (FieldDoesNotExist, NotUniqueError,
                                 DoesNotExist, ValidationError,
                                 InvalidQueryError)
-
+import json
 from resources.errors import (SchemaValidationError, AuthorAlreadyExistsError,
                               InternalServerError, UpdatingAuthorError,
                               DeletingAuthorError, AuthorNotExistsError)
@@ -14,8 +14,15 @@ from resources.errors import (SchemaValidationError, AuthorAlreadyExistsError,
 
 class AuthorsApi(Resource):
     def get(self):
-        authors = Author.objects().to_json()
-        return Response(authors, mimetype="application/json", status=200)
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 3))
+        paginator = Pagination(Author.objects, page, per_page)
+
+        # TODO Return [] if page does not exist
+        # TODO Return paginator properties
+        authors_json = [i.to_json() for i in paginator.items]
+
+        return Response(authors_json, mimetype="application/json", status=200)
 
     def post(self):
         try:
